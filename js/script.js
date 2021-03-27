342fa2ba6d7d3b4c0d5071887d0655724f5a5632
Mutte Chen <mutte109@gmail.com>
2021-03-26 17:13:21 -0700

    Added gallery onclick functionality

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* Single Product */
+
+function imageShowcase1() {
+	document.getElementById("image-showcase").src = "assets/img/product/vac1.jpg";
+}
+
+function imageShowcase2() {
+	document.getElementById("image-showcase").src = "assets/img/product/vac2.jpg";
+}
+
+function imageShowcase3() {
+	document.getElementById("image-showcase").src = "assets/img/product/vac3.jpg";
+}
+
+function imageShowcase4() {
+	document.getElementById("image-showcase").src = "assets/img/product/vac4.jpg";
+}
