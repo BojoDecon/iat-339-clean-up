b4e26217d5cfa0eb5aec7dc2c9d22a102c33019a
Annette <annette0chen@gmail.com>
2021-03-26 17:35:12 -0700

    add side filter and sort by filter functionality
    on product list page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /* Single Product */
 
 function imageShowcase1() {
@@ -14,4 +16,61 @@ function imageShowcase3() {
 
 function imageShowcase4() {
 	document.getElementById("image-showcase").src = "assets/img/product/vac4.jpg";
+}
+
+/*Product list side filter accordion*/
+/*Reference: https://www.w3schools.com/howto/howto_js_accordion.asp*/
+/*Category accordion*/
+let categoryBtn = document.getElementById("category-btn");
+categoryBtn.addEventListener("click", function(){
+    this.classList.toggle("active");
+    
+    let panel = this.nextElementSibling;
+    let iconIndex="category-chevron";
+
+    if (panel.style.display === "block"){
+        document.getElementById(iconIndex).setAttribute("name", "chevron-down-outline");
+        panel.style.display = "none";
+        
+    } else {
+        panel.style.display = "block";
+        document.getElementById(iconIndex).setAttribute("name", "chevron-up-outline");
+    }
+});
+/*Features accordion*/
+let featuresBtn = document.getElementById("features-btn");
+featuresBtn.addEventListener("click", function(){
+    this.classList.toggle("active");
+    
+    let panel = this.nextElementSibling;
+    let iconIndex="features-chevron";
+
+    if (panel.style.display === "block"){
+        document.getElementById(iconIndex).setAttribute("name", "chevron-down-outline");
+        panel.style.display = "none";
+        
+    } else {
+        panel.style.display = "block";
+        document.getElementById(iconIndex).setAttribute("name", "chevron-up-outline");
+    }
+});
+
+
+/*Product list Sort By Dropdown*/
+/*Reference: https://www.w3schools.com/howto/howto_js_dropdown.asp*/
+function dropDown() {
+    document.getElementById("sort-by-dropdown").classList.toggle("show");
+}
+  
+window.onclick = function(event) {
+    if (!event.target.matches('.drop-btn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
